10afb42b3821d1f3f4a72c4121d35e8d2f10d3e5
arthurmsantos017 <09santoaarthurmorais@gmail.com>
2026-06-01 12:31:47 +0000

    versao1

--- reative_vazio/vazio/App.js
+++ reative_vazio/vazio/App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text>Teste de tela!</Text>
+      </View>
+      <View Style={styles.cont_texto}>
+        <Text>
+
+        </Text>
+        </View>
+        <View>
+        </View>
+</ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margintop:50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  cont_texto: {
+    height: 500,
+    backgroundColor: "red",
+
   },
 });
